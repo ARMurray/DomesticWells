4e08eeb82db53e475bc474b98ec9585fd5d4d87a
ARMurray <36209610+ARMurray@users.noreply.github.com>
2020-04-23 15:31:05 -0400

    Built iterator for step 3

--- scripts/03_convert_to_densities.R
+++ scripts/03_convert_to_densities.R
@@ -22,21 +22,44 @@ sfEA$Area <- st_area(sfEA)%>%
 sfEA$well_Density <- (sfEA$Drill_sow+sfEA$Dug_sow) / sfEA$Area
 
 
-# Write some temporary files to get this to run faster on longleaf
-#st_write(sfEA, here("data/temp/sfEA_shp"),driver = "ESRI Shapefile")
-#st_write(sfEA, here("data/temp/sfEA_json"),driver = "GeoJSON")
-#st_write(sfEA, here("data/temp/sfEA_kml"),driver = "KML")
+# The next step is to rasterize the data. This is a very computationally intense operation
+# I have listed two options below. The first option is to try to do it all at once, but only
+# attempt this if you are using a very powerful computer. The second option will iterate and
+# rasterize one state at a time, then mosaic everything back together.
+
+####### ALL AT ONCE #######
 
 # Get bounding box of polygons
-extent <- st_bbox(sfEA) 
+#extent <- st_bbox(sfEA) 
 
 # Determine columns and rows based on the extent and a 20 meter cell-size
-rows <- round(as.numeric(extent$ymax - extent$ymin)/20,0)
-cols <- round(as.numeric(extent$xmax - extent$xmin)/20,0)
+#rows <- round(as.numeric(extent$ymax - extent$ymin)/20,0)
+#cols <- round(as.numeric(extent$xmax - extent$xmin)/20,0)
 
-r <- raster(ncol = cols, nrow = rows)
-extent(r) <- extent(sfEA)
+# Create the empty raster
+#r <- raster(ncol = cols, nrow = rows)
+#extent(r) <- extent(sfEA)
 
-plot(r)
+# rasterize the entire country all at once.
+#wellDensity_90 <- rasterize(sfEA,r, field = 'well_Density')
 
-rr <- rasterize(sfEA,r, field = 'well_Density')
+
+####### ONE STATE AT A TIME #######
+
+# Create list of all state fips codes
+states <- levels(sfEA$ST_FIPS)
+
+# Create for loop
+for(n in states){
+  sub <- sfEA%>%
+    filter(ST_FIPS == n)
+  print(paste0("Starting ",n," at: ",Sys.time()))
+  extent <- st_bbox(sub)
+  rows <- round(as.numeric(extent$ymax - extent$ymin)/20,0)
+  cols <- round(as.numeric(extent$xmax - extent$xmin)/20,0)
+  r <- raster(ncol = cols, nrow = rows)
+  extent(r) <- extent(sub)
+  rast <- rasterize(sub,r, field = 'well_Density')
+  writeRaster(rast, paste0(here("data/rasters/well_density_90"),"/well_density_90_",n,".tif")) # Write the raster to a folder
+  print(paste0("Finished ",n," at: ",Sys.time()))
+}
